f7f1e6fed7ae5501d1863bb83533a4c92469654f
BrandedNomad <info@brandednomad.com>
2021-01-05 11:39:04 +0700

    initial commit

---
+++ src/controllers/v0/index.router.ts
@@ -0,0 +1,28 @@
+/**
+ * @overview This file contains the following functions and routes
+ *
+ * Middleware:
+ *
+ * Feed: Connects to the feed routes
+ * Users: Connects to the users and auth routes
+ *
+ * Routes
+ *
+ * Version (get / ) returns the routes version
+ *
+ */
+
+import {Router, Request, Response} from 'express';
+import {FeedRouter} from './feed/routes/feed.router';
+import {UserRouter} from './users/routes/user.router';
+
+const router: Router = Router();
+
+router.use('/feed', FeedRouter);
+router.use('/users', UserRouter);
+
+router.get('/', async (req: Request, res: Response) => {
+  res.send(`V0`);
+});
+
+export const IndexRouter: Router = router;
